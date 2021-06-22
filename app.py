4285e8fa99cb39473ac6e1f7b2661a181a6667e8
radekwarowny <radekwarownydev@gmail.com>
2021-06-22 13:32:58 +0100

    updated DATABASE URL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
